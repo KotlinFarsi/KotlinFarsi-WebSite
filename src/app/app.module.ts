69b6e25f33fc523da4fb9415dccd47da717486dd
Sina-Darvishi <sinadarvi@gmail.com>
2019-02-23 09:56:49 +0330

    KotlinFarsi-WebSite v0.1.2 - ngx-md added and used, i have to find a way to change the paths in the context

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMdModule } from 'ngx-md';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgxMdModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
