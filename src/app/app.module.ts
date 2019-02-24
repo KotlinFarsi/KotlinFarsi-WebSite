eafdff385fe1912eef2cd8256f4832fb58e6ad2b
Sina-Darvishi <sinadarvi@gmail.com>
2019-02-24 11:50:09 +0330

    KotlinFarsi-WebSite v0.1.3 - ngx-md added locally for some changes in future so i can proper it for the site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgxMdModule } from 'ngx-md';
+import { NgxMdModule } from 'libs/ngx-md/src/lib/ngx-md.module';
 
 @NgModule({
   declarations: [
